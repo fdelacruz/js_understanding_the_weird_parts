a542400a14d9e689f9a9d106dc8a0f6ccb92b946
Francisco De La Cruz <helloquico@gmail.com>
2015-04-28 00:03:33 -0400

    Existence and Booleans

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-console.log(3 < 2 < 1); // true, left-associativity 
-						// (false < 1) -> true, false coerced to be 0
-						// 0 < 1 = true
+var a;
 
-var a = 0;
-var b = false;
+// goes to internet and looks for a value
 
-if (a === b) {          // does not coerce
-	console.log('They are equal!');
-} else {
-	console.log('Nope, not equal.');
+if (a) {
+	console.log('Something is there.');
 }
