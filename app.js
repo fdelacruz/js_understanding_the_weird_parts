18364152b3f06d041423d8f3699f3269fe78c5b9
Francisco De La Cruz <helloquico@gmail.com>
2015-04-27 23:41:45 -0400

    Comparison Operators

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var a = 1 + '2'; // "12"
-console.log(a);  // typeof a -> "string"
+console.log(3 < 2 < 1); // true, left-associativity 
+						// (false < 1) -> true, false coerced to be 0
+						// 0 < 1 = true
+
+var a = 0;
+var b = false;
+
+if (a === b) {          // does not coerce
+	console.log('They are equal!');
+} else {
+	console.log('Nope, not equal.');
+}
