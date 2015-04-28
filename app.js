86b1706c8f3c1ec478cc491768d9541c1dd3b556
Francisco De La Cruz <helloquico@gmail.com>
2015-04-27 21:32:37 -0400

    Coercion

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-// var a = 3 + 4 * 5;
-// console.log(a); // 23
-
-// var a = 2, b = 3, c = 4;
-
-// a = b = c; // right-to-left associativity, a = 4
-
-// console.log(a); // 4
-// console.log(b); // 4
-// console.log(c); // 4
-
-var a = (3 + 4) * 5; // grouping
-console.log(a); // 35
+var a = 1 + '2'; // "12"
+console.log(a);  // typeof a -> "string"
