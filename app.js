f17921e57eee3d6319dc55061601aa5b4b59d33a
Francisco De La Cruz <helloquico@gmail.com>
2015-05-06 19:58:06 -0400

    Function Factories

--- app.js
+++ app.js
@@ -1,58 +1,21 @@
-function buildFunctions() {
+function makeGreeting(language) {
 
-	var arr = [];
+	return function(firstname, lastname) {
 
-	for (var i = 0; i < 3; i++) {
+		if (language === 'en') {
+			console.log('Hello ' + firstname + ' ' + lastname);
+		}	
 
-		arr.push(
-			function() {   
-				console.log(i);   // function NOT being executed here
-			}
-		);
-	}
+		if (language === 'es') {
+			console.log('Hola ' + firstname + ' ' + lastname);
+		}
 
-	return arr;
+	};
+
 }
 
-var fs = buildFunctions();
+var greetEnglish = makeGreeting('en');
+var greetSpanish = makeGreeting('es');
 
-fs[0](); // 3 because i = 3 at the poing of execution, the end of the loop
-fs[1](); // 3 ...   
-fs[2](); // 3 ...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function buildFunctions2() {
-
-	var arr = [];
-
-	for (var i = 0; i < 3; i++) {
-
-		arr.push(
-			(function(j) { // 0, 1, 2..
-				return function() {
-					console.log(j);
-				}
-			}(i)) 
-		);
-	}
-
-	return arr;
-}
-
-var fs2 = buildFunctions2();
-
-fs2[0](); // 0 because j retains its value at its point of execution
-fs2[1](); // 1 ...
-fs2[2](); // 2 ...
+greetEnglish('John', 'Doe');
+greetSpanish('John', 'Doe');
