33954c20c7e9a97e783a6aa6ecbb966d12de2508
Francisco De La Cruz <helloquico@gmail.com>
2015-05-06 08:48:33 -0400

    Arrays - Collections of Anything

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-function a() {
-	console.log(this);
-	this.newvariable = 'hello';
-}
+var arr = [
+	1,
+	false,
+	{
+		name:'Tony',
+		address: '111 Main St.'
+	},
+	function(name) {
+		var greeting = 'Hello ';
+		console.log(greeting + name);
+	},
+	"hello"
+];
 
-var b = function() {
-	console.log(this);
-};
+console.log(arr);
+arr[3](arr[2].name); // Hello Tony
 
-a();
-
-console.log(newvariable);
-
-b();
-
-var c = {
-	name: 'The c object',
-	log: function() {
-		var self = this; 		// both point to the c object
-
-		self.name = 'Updated c object';
-		console.log(self);
-
-		var setname = function(newname) {
-			self.name = newname;
-		};
-		setname('Updated again! The c object');
-		console.log(self);
-	}
-};
-
-c.log();
