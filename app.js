f26569b19bc1c6c3336b09a32b9d507f032f697a
Francisco De La Cruz <helloquico@gmail.com>
2015-05-06 19:10:58 -0400

    Understanding Closures - Part 2

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
-function greet(whattosay) {
+function buildFunctions() {
 
-	return function(name) {
-		console.log(whattosay + ' ' + name);
-	};
+	var arr = [];
 
+	for (var i = 0; i < 3; i++) {
+
+		arr.push(
+			function() {   
+				console.log(i);   // function NOT being executed here
+			}
+		);
+	}
+
+	return arr;
 }
 
-// greet('Hi')('Tony');
-var sayHi = greet('Hi'); // Same thing
-sayHi('Tony');
+var fs = buildFunctions();
+
+fs[0](); // 3 because i = 3 at the poing of execution, the end of the loop
+fs[1](); // 3 ...   
+fs[2](); // 3 ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function buildFunctions2() {
+
+	var arr = [];
+
+	for (var i = 0; i < 3; i++) {
+
+		arr.push(
+			(function(j) { // 0, 1, 2..
+				return function() {
+					console.log(j);
+				}
+			}(i)) 
+		);
+	}
+
+	return arr;
+}
+
+var fs2 = buildFunctions2();
+
+fs2[0](); // 0 because j retains its value at its point of execution
+fs2[1](); // 1 ...
+fs2[2](); // 2 ...
