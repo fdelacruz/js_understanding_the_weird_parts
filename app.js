d8d1da73cbc3c8e8fea7c5cff1b3f03ab33d6bfb
Francisco De La Cruz <helloquico@gmail.com>
2015-05-07 18:39:17 -0400

    bind(), call() and apply()

--- app.js
+++ app.js
@@ -1,31 +1,60 @@
-function sayHiLater() {
+var person = { 
+	firstname: 'John',
+	lastname: 'Doe',
+	getFullName: function() {
+
+		var fullname = this.firstname + ' ' + this.lastname;
+		return fullname;
+
+	}
+};
+
+var logName = function(lang1, lang2) {
 	
-	var greeting = 'Hi!';
+	console.log('Logged: ' + this.getFullName());
+	console.log('Arguments: ' + lang1 + ' ' + lang2);
+	console.log('-----------');
 
-	setTimeout(function() {
-		
-		console.log(greeting);
+};
 
-	}, 3000);
+// bind() makes a copy of the function
+var logPersonName = logName.bind(person);
+logPersonName('es', 'en');
 
+// call() executes the function
+logName.call(person, 'en', 'es');
+
+// apply() uses arrays as arguments
+logName.apply(person, ['es', 'en']);
+
+// apply() using an IIFE
+(function(lang1, lang2) {
+
+	 console.log('Logged: ' + this.getFullName());
+	 console.log('Arguments: ' + lang1 + ' ' + lang2);
+	 console.log('-----------');
+	 
+ }).apply(person, ['en', 'es']);
+
+// function borrowing (using apply() or call())
+var person2 = {
+	firstname: 'Jane',
+	lastname: 'Doe'
 }
 
-sayHiLater();
+console.log(person.getFullName.apply(person2));
 
+// function currying (with bind() which makes a copy of the function)
+function multiply(a, b) {
 
-function tellMeWhenDone(cb) {
-
-	console.log(1); // some work
-	console.log(2); // some work
-	console.log(3); // some work
-
-	cb(); // the 'callback', it runs the function I give it
+	return a*b;
+	
 }
 
-tellMeWhenDone(function() {
-	console.log('I am done!');
-});
+console.log('-----------');
 
-tellMeWhenDone(function() {
-	console.log('All done!');
-});
+var multipleByTwo = multiply.bind(this, 2); // permanently setting a to 2
+console.log(multipleByTwo(4));
+
+var multipleByTwo = multiply.bind(this, 3); // permanently setting a to 3
+console.log(multipleByTwo(4));
