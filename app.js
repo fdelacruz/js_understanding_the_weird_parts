c4c96e3467350c38181474dd70783260b444b1fb
Francisco De La Cruz <helloquico@gmail.com>
2015-05-08 14:39:50 -0400

    Function Constructors and '.prototype'

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// Function Constructors
-// function Person() {
-	
-// 	console.log(this); 		 // Object {} being returned
-// 	this.firstname = 'John'; // Object {firstname: "John"}
-// 	this.lastname = 'Doe'; 	 // Object {firstname: "John", lastname: "Doe"}
-// 	console.log('This function is invoked');
-	
-// 	// if I return something else the original Object doesn't get returned
-// 	// return { greeting: 'I got in the way!' }; // Object {greeting: "I got in the way!"}
-// }
-
-// var john = new Person();
-// console.log(john);
-
-// var jane = new Person({
-// 	firstname: 'Jane',
-// 	lastname: 'Doe'
-// });
-// console.log(jane);
-
-// Setting parameters 
 function Person(firstname, lastname) {
 	
 	console.log(this); 		 
@@ -29,8 +7,18 @@ function Person(firstname, lastname) {
 	
 }
 
+Person.prototype.getFullName = function() {
+	return this.firstname + ' ' + this.lastname;
+};
+
 var john = new Person('John', 'Doe');
 console.log(john);
 
 var jane = new Person('Jane', 'Doe');
 console.log(jane);
+
+Person.prototype.getFormalName = function() {
+	return  this.lastname + ', ' + this.firstname;
+};
+
+console.log(john.getFormalName());
