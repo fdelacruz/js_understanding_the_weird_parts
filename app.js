c66f1bbdf73aaf8514b92d3d21bd9fcb6b392dfa
Francisco De La Cruz <helloquico@gmail.com>
2015-05-08 18:57:41 -0400

    Arrays and for..in

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-String.prototype.isLengthGreaterThan = function(limit) {
-	return this.length > limit;
-};
+Array.prototype.myCustomFeature = 'Cool!';
 
-console.log('John'.isLengthGreaterThan(2)); //  converted string primitive to String object
+var arr = ['John', 'Jane', 'Jim'];
 
-Number.prototype.isPositive = function() {
-	return this > 0;
-};
+for (var prop in arr) {
+	console.log(prop + ': ' + arr[prop]);
+}
+// The for..in loop will iterate over all enumerable properties of the object
+// itself and those the object inherits from its constructor's prototype.
+// 0: John
+// 1: Jane
+// 2: Jim
+// myCustomFeature: Cool! // inherited property
 
-// console.log(1.isPositive()); // Error: number primitives don't get converted
-
-var a = new Number(1);
-console.log(a.isPositive());
+for (var i = 0; i < arr.length; i++) {
+	console.log(i + ': ' + arr[i]);
+}
+// The regular for loop enumerates only the elements of the array.
+// 0: John
+// 1: Jane
+// 2: Jim
