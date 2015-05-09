3ebce2c84812260e4800ad664debf6258af86a6d
Francisco De La Cruz <helloquico@gmail.com>
2015-05-09 15:10:26 -0400

    Deep Dive into Source Code: jQuery

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-function logNewPerson() {
-	"use strict";
-	
-	var person2;
-	persom2 = {}; // Uncaught ReferenceError: persom2 is not defined
-	console.log(persom2);
-}
-
-var person;
-persom = {};
-console.log(persom);
-logNewPerson(); 
+var q = $("ul.people li");
+console.log(q);
